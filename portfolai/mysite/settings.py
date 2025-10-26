2108a8702536f251b0f96505f11ebefdff64badb
Elizandro I Duran <188512019+eduran04@users.noreply.github.com>
2025-10-25 19:44:06 -0600

    Update settings.py

--- portfolai/mysite/settings.py
+++ portfolai/mysite/settings.py
@@ -27,9 +27,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-if not SECRET_KEY:
-    raise ValueError("SECRET_KEY environment variable must be set")
+SECRET_KEY = 'django-insecure-z4ih%dt*unw7*5(t*hv7(uur(cx1utk@*#yj2rk(m1hn(i@_-g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
