d597db318227a648fdccc4e4f409f035a9f55df2
Elizandro I Duran <188512019+eduran04@users.noreply.github.com>
2025-11-06 04:46:41 -0700

    refactor login and signup logic flow

--- portfolai/core/urls.py
+++ portfolai/core/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
 
