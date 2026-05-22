1e01abecb489688f76735657b1b1f10a2f5b6f42
Elizandro I Duran <188512019+eduran04@users.noreply.github.com>
2026-05-21 19:29:20 -0600

    demo login pass

--- portfolai/core/views/auth.py
+++ portfolai/core/views/auth.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 Demo authentication views.
 """
 from django.contrib.auth.views import LoginView
@@ -6,10 +6,12 @@ from django.conf import settings
 
 
 class DemoLoginView(LoginView):
-    """Login view that exposes the demo username in the template context."""
+    """Login view that exposes demo credentials in the template context."""
     template_name = 'registration/login.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['demo_username'] = settings.DEMO_USERNAME
+        context['demo_password'] = settings.DEMO_PASSWORD
         return context
+
